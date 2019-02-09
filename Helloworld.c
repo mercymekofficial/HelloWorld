85a27bd112a855c3d881b93f048b402245e7ae4f
mercymekofficial <45404287+mercymekofficial@users.noreply.github.com>
2019-02-09 19:18:19 +0300

    Create Helloworld.c

---
+++ Helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// Every code works in a function. No code is able to work without a function. 
+// we may use "void" before any function to describe that it will not return any value.
+
+void main()
+{
+	printf("Hello World!"); // it is going to print this string on the screen. The logic behind is can be thought as send this string between
+  // the brackets to the printf function and it is going to print them to the screen.
+	getch(); // We use this to see the string before the application turns off. This is the first glance that "computers" are not 
+  // human beings they DO NOT always share the same common already coded knowledge like "hmm. if i just printf this string it is going to stand here until anything comes out." No it won't.
+}
